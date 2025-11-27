fef7277f12cc2fb04e7feccbabe9df0f99f4a6bb
Jozeph <jozeppe1997@gmail.com>
2025-11-27 18:10:11 +0300

    Изменение карточки товара

--- src/components/tkanitem/TkanItem.jsx
+++ src/components/tkanitem/TkanItem.jsx
@@ -14,7 +14,7 @@ const {tkans} = useContext(Context)
         
             <Card>
             <div className="bg-white rounded-lg group
-                            p-3 cursor-pointer h-[calc(100%-40px)] hover:h-full">   
+                            p-3 cursor-pointer h-[calc(100%-90px)] hover:h-full">   
                 <img src={tkan.img}/>
                 <h6 className="text-black font-semibold">{tkan.name}</h6>
                 <h5 className="text-accent text-b_fone font-semibold">{tkan.price} ₽/м</h5>
@@ -22,6 +22,14 @@ const {tkans} = useContext(Context)
                 <div className="opacity-0 transform transition-all duration-300
                                 group-hover:opacity-100"
                 >
+                    <div className="w-auto h-[40px] flex justify-between items-center rounded-lg">
+                        <div className="flex flex-row">
+                            <button className="px-3 py-1 text-lg">-</button>
+                            <h5 className="text-[#101010] px-4 py-1 bg-[#E4E2DE]">1.0</h5>
+                            <button className="px-3 py-1 text-lg">+</button>
+                        </div>
+                        <h5 className="text-accent text-[#101010] font-semibold">{tkan.price} ₽/м</h5>
+                    </div>
                     <Button_1l/>
                 </div>
             </div> 
