4a841fdf713b1cbc5e654e49e1e1c914b6cf247f
Jozeph <jozeppe1997@gmail.com>
2025-11-27 18:24:11 +0300

    Mobile button

--- src/components/tkanitem/TkanItem.jsx
+++ src/components/tkanitem/TkanItem.jsx
@@ -3,7 +3,7 @@ import styles from "./TkanItem.module.css"
 import { useContext } from "react"
 import { Context } from "../../main"
 import { Card } from "@radix-ui/themes"
-import { Button_1l } from "../button/Button"
+import { Button_1l, Basket_Icon } from "../button/Button"
 
 
 export let TkanItem = ({tkan}) =>{
@@ -14,28 +14,34 @@ const {tkans} = useContext(Context)
         
             <Card>
             <div className="bg-white rounded-lg group
-                            p-3 cursor-pointer h-[calc(100%-90px)] hover:h-full">   
+                            p-3 cursor-pointer md:h-[calc(100%-90px)] hover:h-full">   
                 <img src={tkan.img}/>
                 <h6 className="text-black font-semibold">{tkan.name}</h6>
                 <h5 className="text-accent text-b_fone font-semibold">{tkan.price} ₽/м</h5>
-
+                <div className="md:hidden">
+                        <Basket_Icon/>
+                </div>
                 <div className="opacity-0 transform transition-all duration-300
                                 group-hover:opacity-100"
                 >
-                    <div className="w-auto h-[40px] flex justify-between items-center rounded-lg">
-                        <div className="flex flex-row">
+                    <div className="md:hidden"></div>
+                    <div className="w-auto h-[40px] justify-between items-center rounded-lg hidden md:block">
+                        <div className="flex flex-row ">
                             <button className="px-3 py-1 text-lg">-</button>
                             <h5 className="text-[#101010] px-4 py-1 bg-[#E4E2DE]">1.0</h5>
                             <button className="px-3 py-1 text-lg">+</button>
                         </div>
                         <h5 className="text-accent text-[#101010] font-semibold">{tkan.price} ₽/м</h5>
                     </div>
-                    <Button_1l/>
+                    <div className="hidden md:block">
+                        <Button_1l/>
+                    </div>
+                    
                 </div>
+                
+                
             </div> 
             </Card>
-        
-        
         </>
     )
 }
