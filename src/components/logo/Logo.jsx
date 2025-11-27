52600198e3f344b5174381f2204bc224dc8edc26
Jozeph <jozeppe1997@gmail.com>
2025-11-27 14:49:39 +0300

    Добавлена навигация через логотип

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,13 +1,18 @@
 import styles from "./Logo.module.css"
+import { NavLink } from "react-router-dom";
+import { SHOP_ROUTE } from "../../utils/consts";
 
 export let Logo = () =>{
 
     return(
         <>
+            <NavLink to={SHOP_ROUTE}>
                 <div className="flex gap-[6.48px]">
                     <img className="w-[32px] md:w-[36px]" src="./Logo Icon.svg" alt="LOGO"/>
                     <img className="w-[105px] md:w-[120px]" src="./CENTER TKANI.svg" alt="LOGO"/>
                 </div>
+            </NavLink>
+                
         </>
     );
 }
