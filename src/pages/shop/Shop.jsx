ebff644f90b64d9d6c0d0fa373b70733ffb6867b
Jozeph <jozeppe1997@gmail.com>
2025-11-28 13:46:50 +0300

    create shoppage

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,24 +1,10 @@
-import { observer } from "mobx-react-lite"
 import styles from "./Shop.module.css"
-import { Context } from "../../main";
-import { useState, useRef, useContext } from "react";
-import { Tkanlist } from "../../components/tkanlist/TkanList";
-import { Slider } from "../../components/slider/Slider"
 
-export let Shop = observer(() =>{
-    const {tkans} = useContext(Context)
+export let Shop = () =>{
+
     return(
         <>
-        <div className="mx-2 flex flex-col md:flex-row">
-            
-            <Slider Name={"одежды"}/>
-            <Slider Name={"Дома"}/>
-            
-        </div>
-        
-       <Tkanlist/>
-
-        
+        Shop
         </>
-    )
-})
+    );
+}
