b40126e10beb6f444eb940a66def9d92613bb8f3
Jozeph <jozeppe1997@gmail.com>
2025-11-28 13:43:07 +0300

    change Navlink

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,12 @@
 import styles from "./Logo.module.css"
 import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../../utils/consts";
+import { MAIN_PAGE } from "../../utils/consts";
 
 export let Logo = () =>{
 
     return(
         <>
-            <NavLink to={SHOP_ROUTE}>
+            <NavLink to={`${MAIN_PAGE}#main`}>
                 <div className="flex gap-[6.48px]">
                     <img className="w-[32px] md:w-[36px]" src="./Logo Icon.svg" alt="LOGO"/>
                     <img className="w-[105px] md:w-[120px]" src="./CENTER TKANI.svg" alt="LOGO"/>
