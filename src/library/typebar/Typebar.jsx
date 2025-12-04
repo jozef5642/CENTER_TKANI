ac0cda7ae771299c260fe0418239460851e33b32
Jozeph <jozeppe1997@gmail.com>
2025-12-04 14:45:51 +0300

    change styles

--- src/library/typebar/Typebar.jsx
+++ src/library/typebar/Typebar.jsx
@@ -1,24 +1,24 @@
 import { observer } from "mobx-react-lite"
 import styles from "./Typebar.module.css"
 import { Context } from "../../main";
-import { useState, useRef, useContext } from "react";
+import { useContext } from "react";
 
 export const Typebar = observer(({ Styles }) => {
     const { tkans } = useContext(Context);
 
+    
+
     return (
-        <div className={`flex ${Styles} gap-2`}>
-            {tkans.types?.map(type => (
+        <div className={`${Styles}`}>
+            {tkans.types.map(type => (
                 <ul
-                    className="bg-[#F1F0EE] rounded-lg text-black 
-                               text-nowrap w-[293px] text-center text-[17px] cursor-pointer"
+                    onClick={() => tkans.setSelectedType(type)}
+                    className={`bg-[#F1F0EE] rounded-lg  
+                                text-nowrap w-[293px] text-center text-[17px] cursor-pointer 
+                                ${tkans.selectedType.id === type.id ? "text-b_fone" : "text-black"}`}
                     key={type.id}
                 >
                     {type.name}
-
-                    {tkans.brands?.map(brand => (
-                        <li key={brand.id}>{brand.name}</li>
-                    ))}
                 </ul>
             ))}
         </div>
