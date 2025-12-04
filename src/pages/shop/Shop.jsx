2c95f07efdf2cc235e28c8f84d3923e6df6099ac
Jozeph <jozeppe1997@gmail.com>
2025-12-04 14:47:36 +0300

    create shopPage

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,10 +1,14 @@
 import styles from "./Shop.module.css"
-
+import { Typebar } from "../../library/typebar/Typebar"
+import { BrandBar } from "../../library/brandBar/BrandBar";
+import { Tkanlist } from "../../components/tkanlist/TkanList";
 export let Shop = () =>{
 
     return(
         <>
-        Shop
+        <Typebar/>
+        <BrandBar/>
+        <Tkanlist/>
         </>
     );
 }
