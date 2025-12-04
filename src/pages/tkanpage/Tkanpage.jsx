b87e3e779bc48c966648d5ba96594bd5425c7f2e
Jozeph <jozeppe1997@gmail.com>
2025-12-04 18:09:28 +0300

    added scroll

--- src/pages/tkanpage/Tkanpage.jsx
+++ src/pages/tkanpage/Tkanpage.jsx
@@ -1,12 +1,14 @@
 import { Breadcrumbs } from "../../components/breadcrumbs/Breadcrumbs"
 import styles from "./Tkanpage.module.css"
 import image1 from "../../../public/image_wet_asphalt_1.png"
+import { useScrollToHash } from "../../hooks/useScrollToHash"
 
 export let Tkanpage = () => {
     const tkan = { id: 1, name: 'Двунитка Мокрый Асфальт', price: 500, rating: 5, img: image1 }
+    useScrollToHash();
     return (
         <>
-            <section className="flex flex-col justify-center items-center gap-1 m-2">
+            <section id="target" className="flex flex-col justify-center items-center gap-1 m-2">
                 <img className="w-[372px] h-[480px] rounded-xl" key={tkan.id} src={tkan.img} alt={tkan.name} />
                 <div className="flex gap-2 justify-between">
                     
