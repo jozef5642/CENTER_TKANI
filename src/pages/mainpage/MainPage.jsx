d35add5fbfccc4082f44ff7a1e3ada124f4cb327
Jozeph <jozeppe1997@gmail.com>
2025-12-04 17:37:06 +0300

    delete scroll

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,36 +1,16 @@
 import { observer } from "mobx-react-lite"
 import styles from "./MainPage.module.css"
 import { Context } from "../../main";
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { Tkanlist } from "../../components/tkanlist/TkanList";
 import { Slider } from "../../components/slider/Slider"
 import { Typebar } from "../../library/typebar/Typebar";
-import { useLocation } from "react-router-dom";
 import { Button_TextVersion } from "../../components/button/Button";
+
 
 export let MainPage = observer(() => {
     const { tkans } = useContext(Context)
 
-
-    const { hash } = useLocation();
-
-    useEffect(() => {
-        if (hash && document.visibilityState === "visible") {
-
-            const id = hash.replace("#", "");
-            const el = document.getElementById(id);
-
-            if (el) {
-
-                const elementPosition = el.getBoundingClientRect().top + window.pageYOffset;
-
-                window.scrollTo({
-                    top: elementPosition - 100,
-                    behavior: "smooth",
-                });
-            }
-        }
-    }, [hash, 100]);
 
     return (
         <>
