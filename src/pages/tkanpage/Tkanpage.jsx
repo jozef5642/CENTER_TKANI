bd407513c3341ac043c2a87e486af6071f68edd3
Jozeph <jozeppe1997@gmail.com>
2025-12-05 12:28:50 +0300

    change calculator

--- src/pages/tkanpage/Tkanpage.jsx
+++ src/pages/tkanpage/Tkanpage.jsx
@@ -15,9 +15,11 @@ export let Tkanpage = () => {
     useScrollToHash();
 
     const [current, setCurrent] = useState(tkan.img[0]);
+    const [totalPrice, setTotalPrice] = useState(0);
 
     return (
         <>
+            {/* ProductGallery */}
             <div className="xl:flex justify-between mx-2">
                 <section id="target" className="flex flex-col justify-center items-center">
                     <div className="w-full">
@@ -52,15 +54,22 @@ export let Tkanpage = () => {
 
                 <section className="">
                     <div className="">
-                        <h1>{tkan.name}</h1>
-                        <h3>Артикул:{tkan.id}</h3>
-                        <CostCalculator price={tkan.price} />
+                        <h1 className="text-[26px] font-semibold">{tkan.name}</h1>
+                        <h3 className="text-[14px] text-[#888888]">Артикул:{tkan.id}</h3>
+                        <div className="flex">
+                            <CostCalculator
+                                price={tkan.price}
+                                onPriceChange={setTotalPrice}
+                            />
+                            <div>
+                                <p className="w-auto text-[16px] text-end text-[#10101099] text-nowrap m-0 p-0">{tkan.price} ₽ /м</p>
+                                <p className="w-auto text-[26px] text-b_fone font-semibold m-0 p-0 text-nowrap">{totalPrice.toFixed(0)} ₽</p>
+                            </div>
+                        </div>
                         <div className="flex flex-col gap-3 md:flex-row my-1">
                             <Button_2l name={`Добавить в корзину`} />
                             <Button_1l name={`Добавить в один клик`} />
                         </div>
-
-
                     </div>
                 </section>
             </div>
